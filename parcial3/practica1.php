a61278a889b76a73f3695068e7d7bcb09400a42f
Hommeyy <124410599+Hommeyy@users.noreply.github.com>
2023-05-22 18:58:36 -0600

    22/05/23

---
+++ parcial3/practica1.php
@@ -0,0 +1,23 @@
+<?php
+
+
+   echo"Hola Mundo";
+
+   echo "<hr>";
+
+   $nombre = "Luis";
+
+   $edad = 18;
+
+   if($edad > 18){
+    echo $nombre . " es mayor de edad";
+   } {
+    echo $nombre . " es menor de edad";
+   }
+   echo "<br><br>";
+
+   for($i=0; $i<10; $i++){
+    echo $i . "<br>";
+   }
+
+?>
